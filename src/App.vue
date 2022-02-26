a64b2c90b3158c2ab68f9f852c737e3196eb2676
Wenish <Wenish@users.noreply.github.com>
2022-02-26 18:46:14 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div ref="game"></div>
+  <div ref="gameCanvas"></div>
 </template>
 <script setup lang="ts">
 import * as Matter from 'matter-js'
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 
-const game = ref<HTMLElement>()
-
-const createPlayer = (): Matter.Body => {
-  return Matter.Bodies.rectangle(0, -10, 10, 10, {
+const createPlayer = (x: number, y: number): Matter.Body => {
+  return Matter.Bodies.rectangle(x, y, 10, 10, {
     friction: 0.9
   });
 }
@@ -17,37 +15,36 @@ const createGround = (): Matter.Body => {
   return Matter.Bodies.rectangle(0, 30, 1000, 60, { isStatic: true });
 }
 
+const gameCanvas = ref<HTMLElement>()
+// create an engine
+const engine = Matter.Engine.create();
+const ground = createGround()
+
+// add all of the bodies to the world
+Matter.Composite.add(engine.world, ground);
+
+// create runner
+const runner = Matter.Runner.create();
+
+// run the engine
+Matter.Runner.run(runner, engine);
+
+const players = [createPlayer(-10, -10), createPlayer(20, -10)]
+Matter.Composite.add(engine.world, players);
+
 onMounted(() => {
-  console.log(game.value)
-  // module aliases
-
-  // create an engine
-  var engine = Matter.Engine.create();
-
   // create a renderer
-  var render = Matter.Render.create({
-    element: game.value,
+  const render = Matter.Render.create({
+    element: gameCanvas.value,
     engine: engine
   });
-
-  const players = [createPlayer(), createPlayer()]
-
-  var ground = createGround()
-
-  // add all of the bodies to the world
-  Matter.Composite.add(engine.world, ground);
-  Matter.Composite.add(engine.world, players);
 
   // run the renderer
   Matter.Render.run(render);
 
-  // create runner
-  var runner = Matter.Runner.create();
-
-  // run the engine
-  Matter.Runner.run(runner, engine);
   Matter.Events.on(engine, 'afterUpdate', () => {
     const highestPlayer = players.reduce((max, game) => max.position.y > game.position.y ? max : game);
+    if (!highestPlayer) return
     Matter.Render.lookAt(render, highestPlayer, {
       x: 10,
       y: 200
