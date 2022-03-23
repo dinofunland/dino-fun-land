425411628a8bbbb238a4e58143bc1ee4506d48db
Wenish <Wenish@users.noreply.github.com>
2022-03-23 02:24:38 +0100

    add background elements

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <router-view :key="$route.path" />
+  <BackgroundElements />
 </template>
-<script setup lang="ts">
+<script setup lang="ts">import { defineAsyncComponent } from 'vue';
+
+const BackgroundElements = defineAsyncComponent(() => import('./components/BackgroundElements.vue'))
 </script>
---
+++ src/components/BackgroundElements.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="container">
+    <div class="element element1"></div>
+    <div class="element element2"></div>
+    <div class="element element3"></div>
+
+    <div class="element element4"></div>
+    <div class="element element5"></div>
+    <div class="element element6"></div>
+</div>
+</template>
+<style scoped>
+.container {
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    pointer-events: none;
+    z-index: -1;
+    background-color: #3e2347;
+}
+.element {
+    background-color: #5a265e;
+    position: absolute;
+    clip-path: polygon(90% 0, 100% 0%, 10% 100%, 0% 100%);
+    width: 400px;
+    height: 400px;
+}
+
+.element1 {
+    bottom: -160px;
+    left: -180px;
+}
+.element2 {
+    bottom: -80px;
+    left: -40px;
+}
+
+.element3 {
+    bottom: -260px;
+    left: -130px;  
+}
+
+.element4 {
+    top: -260px;
+    right: -140px;
+}
+
+.element5 {
+    top: -120px;
+    right: -80px;
+}
+
+.element6 {
+    top: -140px;
+    right: -160px;
+}
+</style>
