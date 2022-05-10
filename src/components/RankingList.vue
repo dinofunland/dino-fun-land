8b1260d6ca87390228337383f693183ec356dc10
Wenish <Wenish@users.noreply.github.com>
2022-05-10 21:07:10 +0200

    feat: show every player once on rank list

--- src/components/RankingList.vue
+++ src/components/RankingList.vue
@@ -4,26 +4,71 @@
             <RankingListFilter v-for="index in 4" :key="index" :isActive="playerCountFilter == index"
                 @click="loadRankList(index)">{{ index }} Player</RankingListFilter>
         </div>
-        <RankingListItem v-for="item in ranking" :key="item.id" :rank="item.rank" :score="item.score"
+        <RankingListItem v-for="(item, index) in rankingList" :key="item.id" :rank="index + 1" :score="item.score"
             :players="item.players" />
         <div v-if="!ranking.length && !scoreRequest?.isFetching" class="no-scores-found">No Scores Found</div>
         <div v-if="scoreRequest?.isFetching">Loading Scores...</div>
     </div>
 </template>
 <script setup lang="ts">
+import { computed } from '@vue/reactivity';
 import { useFetch, UseFetchReturn } from '@vueuse/core';
 import { defineAsyncComponent, ref } from 'vue';
 const RankingListFilter = defineAsyncComponent(() => import('./RankingListFilter.vue'))
 const RankingListItem = defineAsyncComponent(() => import('./RankingListItem.vue'))
 
+const areArraysEqualSets = (a1: any[], a2: any[]) => {
+  const superSet: any = {};
+  for (const i of a1) {
+    const e = i + typeof i;
+    superSet[e] = 1;
+  }
+
+  for (const i of a2) {
+    const e = i + typeof i;
+    if (!superSet[e]) {
+      return false;
+    }
+    superSet[e] = 2;
+  }
+
+  for (let e in superSet) {
+    if (superSet[e] === 1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 const playerCountFilter = ref(1)
 
 const ranking = ref<Array<{
     id: string,
-    rank: number,
     score: number,
     players: string[]
 }>>([])
+
+const rankingList = computed<Array<{
+    id: string,
+    score: number,
+    players: string[]
+}>>(() => {
+    const array: Array<{
+    id: string,
+    score: number,
+    players: string[]
+}> = []
+    for (let rank of ranking.value) {
+        const isRankAlreadyInList = array.some(obj => areArraysEqualSets(obj.players, rank.players))
+        if(isRankAlreadyInList) continue;
+        array.push(rank)
+        if(array.length > 9) {
+            break;
+        }
+    }
+    return array
+})
 
 const scoreRequest = ref<UseFetchReturn<any>>()
 
@@ -44,7 +89,6 @@ const loadRankList = async (playerCount: number) => {
                         })?.sort()
                         return {
                             id: document?.name,
-                            rank: index + 1,
                             score: parseInt(document?.fields?.score?.integerValue),
                             players
                         }
@@ -75,7 +119,7 @@ const loadRankList = async (playerCount: number) => {
                 },
                 direction: 'DESCENDING'
             }],
-            limit: 10
+            limit: 1000
         }
     }).json()
 }
